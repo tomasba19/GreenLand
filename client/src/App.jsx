a6ec843db55853c788c90bbe2cc135abb4cc0ac8
Andrea Laura <andrea_carol00@hotmail.com>
2023-08-15 22:05:33 -0500

    NavBar Implementation
    Following FIGMA Mockup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
 // Este es el componente principal de la app, aquí se renderizan los distintos componentes de la interfaz
 import React from "react";
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { NavBar } from "./components/NavBar";
 
 function App() {
+  const location = useLocation();
 
   return (
     /*Estilos para el scroll-bar, pendiente de modularizar*/
     <div className="container-scroll">
-      <h1>GreenLand</h1>
-    </div>
-    
+
+    {
+      location.pathname !== '/' && <NavBar/>
+    }
+
+    <Routes>
+    </Routes>
+
+    </div>  
   )
 }
 

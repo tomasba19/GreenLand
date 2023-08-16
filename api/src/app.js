3f6963f9749c7ee9aaa0e7a93cec9c9475cf0857
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-08-15 19:16:06 -0500

    fix-error-variable-username-in-config-and-add-main-route

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const routes = require('./routes/index.js')
 
 const app = express()
 
@@ -7,5 +8,14 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 // rutas para la app
+app.use('/', routes)
+
+// Error catching endware.
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500
+  const message = err.message || err
+  console.error(err)
+  res.status(status).send(message)
+})
 
 module.exports = app
--- api/src/database/config.js
+++ api/src/database/config.js
@@ -5,7 +5,7 @@ const db = new Sequelize({
   dialect: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  username: process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   logging: false

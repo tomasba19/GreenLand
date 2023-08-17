b21c37585549bb2292a87c197f0751e8bab7cccf
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-08-17 15:34:43 -0500

    create-detailProduct-route

---
+++ api/src/controllers/products/detailProduct.js
@@ -0,0 +1,17 @@
+const { Product, Category } = require('../../database/config.js')
+
+const detailProduct = async (req, res) => {
+  const { id } = req.params
+  if (!/^\d+$/.test(id)) {
+    return res.status(400).json({ error: 'El ID debe ser un número válido' })
+  }
+  try {
+    const products = await Product.findOne({ where: { id: Number(id) }, include: Category, attributes: { exclude: ['categoryId'] } })
+    if (!products) return res.status(404).json({ error: `El product con id: ${id}, no se encontro` })
+    res.json(products)
+  } catch (error) {
+    res.status(error.response?.status || 500).json({ error: error.message })
+  }
+}
+
+module.exports = detailProduct
--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,7 +1,10 @@
 const allProducts = require('../controllers/products/allProducts.js')
+const detailProduct = require('../controllers/products/detailProduct.js')
 const { Router } = require('express')
 const router = Router()
 
 router.get('/', allProducts)
+
+router.get('/:id', detailProduct)
 
 module.exports = router

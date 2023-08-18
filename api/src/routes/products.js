0f5b405fdcdbf7e375c31c9d120b0485ac305d03
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-08-18 15:38:28 -0500

    add-routes-for-put-and-delete-products

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,6 +1,8 @@
 const allProducts = require('../controllers/products/allProducts.js')
 const detailProduct = require('../controllers/products/detailProduct.js')
 const newProduct = require('../controllers/products/newProduct.js')
+const updateProduct = require('../controllers/products/updateProduct.js')
+const deleteProduct = require('../controllers/products/deleteProduct.js')
 const upload = require('../config/multer.js')
 const { Router } = require('express')
 const router = Router()
@@ -10,5 +12,9 @@ router.get('/', allProducts)
 router.get('/:id', detailProduct)
 
 router.post('/', upload.fields([{ name: 'image', maxCount: 1 }]), newProduct)
+
+router.put('/:id', updateProduct)
+
+router.delete('/:id', deleteProduct)
 
 module.exports = router

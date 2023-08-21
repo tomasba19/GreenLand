8b3a59c8612319040a688ca9fbb4e557b6aabe2f
edisonrmedina <edison1282015@gmail.com>
2023-08-21 00:13:23 -0500

    Update loginUser.js

--- api/src/controllers/users/loginUser.js
+++ api/src/controllers/users/loginUser.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const { User } = require("../../database/config");
 const generateJWT = require("../../utils/jwt");
+const { decodeTokenOauth } = require("../../utils/decodeOauth");
 
 exports.login = async (req, res) => {
     const { email, password } = req.body
@@ -18,6 +19,11 @@ exports.login = async (req, res) => {
         return res.status(401).json({ error: 'Invalid password' })
     }
 
+    if(user.active === false)
+    {
+        return res.status(401).json({ error: 'User inactive' })
+    }
+    
     const token = await generateJWT(user.id)
 
     res.json({
@@ -25,4 +31,67 @@ exports.login = async (req, res) => {
         token
     })
 
+}
+
+exports.signUpGoogle = async (req, res) => {
+    const {tokenId} = req.body
+    const { name, email, picture  } = await decodeTokenOauth(tokenId);
+    
+    const userExist = await User.findOne({
+        where: { email }
+    })
+
+    if (userExist) {
+        return res.status(400).json({ error: 'User already exists' })
+    }
+    
+    const userCreated = await User.create(
+      {
+        name,
+        email,
+        image: picture,
+        roleId: 2,
+        origin: "google",
+      },
+      {
+        attributes: { exclude: ["roleId", "password"] },
+      }
+    );
+    
+    if (!userCreated) {
+      return res.status(404).json({ error: "Error creating user" });
+    }
+
+    const token = await generateJWT(userCreated.id)
+    
+    res.json({
+        user: userCreated,
+        token
+    })
+
+}
+
+exports.loginGoogle = async (req, res) => {
+    const { email } = await decodeTokenOauth(data);
+    
+    const userExist = await User.findOne({
+        where: { email }
+    })
+
+    if (!userExist)
+      return res
+        .status(400)
+        .json({ error: "¡A gmail account is not regiter for this user!" });
+
+    if (userExist.active === false)
+      return res
+        .status(400)
+        .json({ error: "¡A gmail account is not regiter for this user!" });
+
+    const token = await generateJWT(userExist.id)
+
+    res.json({
+        user: userExist,
+        token
+    })
 }

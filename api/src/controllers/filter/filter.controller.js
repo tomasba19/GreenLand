bb521a68628350524c04ffc861c72b3a6dce92c0
Andrea Laura <andrea_carol00@hotmail.com>
2023-08-21 14:55:34 -0500

    connectfilters

--- api/src/controllers/filter/filter.controller.js
+++ api/src/controllers/filter/filter.controller.js
@@ -4,8 +4,6 @@ const { Op } = require('sequelize')
 exports.filterDynamic = async (req, res) => {
   // Filtrar por categoría si se proporciona
   const filterCriterias = req.body
-  console.log('por aqui pipipipipip');
-  console.log(filterCriterias);
   let filteredProducts = await Product.findAll()
   console.log('0', filteredProducts.length)
   if (filterCriterias.categories && filterCriterias.categories.length > 0) {

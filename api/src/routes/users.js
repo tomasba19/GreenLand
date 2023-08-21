61a43b3e508e9a22f47774679f6ba6a2ae8a4d29
edisonrmedina <edison1282015@gmail.com>
2023-08-21 00:11:29 -0500

    Update users.js

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,14 +1,19 @@
 const { Router } = require('express')
 const upload = require('../config/multer.js')
 const createUser = require('../controllers/users/createUser.js')
-const {login} = require('../controllers/users/loginUser.js')
+const userMiddleware = require('../controllers/users/loginUser.js')
 const {protect} = require('../middlewares/auth.middleware.js') // se tiene que pasar un token por authorizacion para pasar el protect
 const { restrictTo } = require("../middlewares/auth.middleware.js"); // el rol nos indica que tipo de usuario puede acceder
 const router = Router()
 
 router
   .post("/", upload.fields([{ name: "image", maxCount: 1 }]), createUser)
-  .post("/login", login); // ejemplo 
+  .post("/login", userMiddleware.login)
+  .post("/signUpGoogle",userMiddleware.signUpGoogle)
+  .post("/loginGoogle",userMiddleware.loginGoogle)
+  
+  
+  // ejemplo 
 //protect, restrictTo("administrator")
 
 module.exports = router

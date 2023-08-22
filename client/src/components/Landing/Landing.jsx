86aecd218fb96cdbf32c472e56ae843e6f942572
Andrea Laura <andrea_carol00@hotmail.com>
2023-08-22 15:12:31 -0500

    landing pseudoimplementation

---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './Landing.module.css';
+
+
+export const Landing = () => {
+    return(
+        <div className={style.landingPage}>
+            <h1 className={style.header}>
+            "The environment is not a resource to be destroyed, but a legacy that we must protect. Join the cause and be an active part of the solution"
+            </h1>
+            <br />
+            <Link className={`${style.btn} ${style.btnPrimary}`} to={"/home"}>
+                JOIN ME.
+            </Link>
+            
+        </div>
+    )
+}

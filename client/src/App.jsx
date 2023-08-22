97804e863c7112a1148c07bc86c44296b36a0391
Andrea Laura <andrea_carol00@hotmail.com>
2023-08-21 21:47:29 -0500

    review real data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-// Este es el componente principal de la app, aquí se renderizan los distintos componentes de la interfaz
 import React from "react";
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { NavBar } from "./components/NavBar/NavBar";
 import { Landing } from "./components/Landing/Landing";
 import { Footer } from "./components/footer/Footer";
@@ -9,13 +8,10 @@ import { Login }  from "./components/Login/Login";
 import { SignUp } from "./components/SignUp/SignUp";
 import { About } from "./components/About/About";
 import { Form } from "./components/Form/Form";
-{/* //borrar despues */}
 import { Detail } from "./components/Detail/Detail";
 import { ContactUs } from "./components/ContactUs/ContactUs"
 
 function App() { 
-  const location = useLocation();
-
   return (
     /*Estilos para el scroll-bar, pendiente de modularizar*/
     <div className="containerScroll">
@@ -27,7 +23,6 @@ function App() {
         <Route path="/signup" element={<SignUp/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/form" element={<Form/>}/>
-        {/* //borrar despues */}
         <Route path="/detail/:id" element={<Detail />}/>
         <Route path="/contact" element={<ContactUs/>}/>
       </Routes>

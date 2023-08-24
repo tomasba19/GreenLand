dddc7630718911a589edd971e1dba8d771156f1c
Tomas Barolo <tomasba19@gmail.com>
2023-08-24 02:49:00 -0300

    Home Fix/Loading added
    Products load fixed. Loading screen implemented

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,24 +2,53 @@ import style from './Home.module.css'
 import ClientComments from '../ClientComments/ClientComments'
 import { NavLink } from 'react-router-dom'
 import {SimpleSlider} from '../Swiper/Swiper'
+import { useEffect, useState } from 'react';
+import { useDispatch } from "react-redux";
+import { getAllProducts } from "../../redux/action";
+import loader from "../../assets/loaderGif.gif"
 
 export const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch              = useDispatch();
+  useEffect(() => {
+    dispatch(getAllProducts());
+
+    Promise.all([dispatch(getAllProducts())])
+    .then(() => {
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error("Error loading data:", error);
+      setLoading(false);
+    });
+  }, [dispatch]);
+
   return (
     <>
-      <div className={style.parent}>
-          <div className={style.div1}>
-              <h1> GreenLand <hr/></h1>
-              <div> Discover Our Eco-Friendly Collection Offering a Diverse Range of Products, Accessories, and Sustainable Alternatives for Every Need and Occasion.
-                    Join the Movement Toward Sustainability with Our Diverse Selection of Eco-Friendly Products, Curated for Eco Warriors Like You. 
-                    From Zero-Waste Essentials to Biodegradable Delights, Our Eco-Conscious Lineup is Designed to Uplift Your Lifestyle and the Planet.
+      {
+        loading === true 
+        ?
+        <div className={style.prodsContLoader}>
+          <img src={loader} alt="Loader"></img>
+        </div>
+        :
+        <>
+          <div className={style.parent}>
+              <div className={style.div1}>
+                  <h1> GreenLand <hr/></h1>
+                  <div> Discover Our Eco-Friendly Collection Offering a Diverse Range of Products, Accessories, and Sustainable Alternatives for Every Need and Occasion.
+                        Join the Movement Toward Sustainability with Our Diverse Selection of Eco-Friendly Products, Curated for Eco Warriors Like You. 
+                        From Zero-Waste Essentials to Biodegradable Delights, Our Eco-Conscious Lineup is Designed to Uplift Your Lifestyle and the Planet.
+                  </div>
+                  <NavLink to="/login" className={style.buttonLink}>LOG IN</NavLink>
               </div>
-              <NavLink to="/login" className={style.buttonLink}>LOG IN</NavLink>
+              <div className={style.div2}> 
+                  <SimpleSlider/>
+              </div>
           </div>
-          <div className={style.div2}> 
-              <SimpleSlider/>
-          </div>
-      </div>
-      <ClientComments/>
+          <ClientComments/>
+          </>
+      }
     </>
   )
 }

1f15fedc82b7db585c47177e2468638af78ab0ee
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-08-27 13:49:49 -0500

    fix-bug-in-the-cart-and-add-modals

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,12 +3,13 @@ import ClientComments from '../ClientComments/ClientComments'
 import { NavLink } from 'react-router-dom'
 import {SimpleSlider} from '../Swiper/Swiper'
 import { useEffect, useState } from 'react';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../../redux/action";
 import loader from "../../assets/loaderGif.gif"
 
 export const Home = () => {
   const [loading, setLoading] = useState(true);
+  const auth = useSelector((state) => state.authData);
   const dispatch              = useDispatch();
   useEffect(() => {
     dispatch(getAllProducts());
@@ -40,7 +41,7 @@ export const Home = () => {
                         Join the Movement Toward Sustainability with Our Diverse Selection of Eco-Friendly Products, Curated for Eco Warriors Like You. 
                         From Zero-Waste Essentials to Biodegradable Delights, Our Eco-Conscious Lineup is Designed to Uplift Your Lifestyle and the Planet.
                   </div>
-                  <NavLink to="/login" className={style.buttonLink}>LOG IN</NavLink>
+                  {!auth && <NavLink to="/login" className={style.buttonLink}>LOG IN</NavLink>}
               </div>
               <div className={style.div2}> 
                   <SimpleSlider/>
--- client/src/components/ShoppingCart/ShoppingCart.jsx
+++ client/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,102 +1,122 @@
-import style from "./ShoppingCart.module.css"
-import { BsFillTrash3Fill } from "react-icons/bs"
+import style from "./ShoppingCart.module.css";
+import { BsFillTrash3Fill } from "react-icons/bs";
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useSelector } from "react-redux";
 
 const { VITE_SERVER_URL } = import.meta.env;
 
 export const ShoppingCart = () => {
-    const authData  = useSelector((state) => state.authData);
-    const [paymentData, setPaymentData] = useState({
-        userId   : authData?.id || null, //!DEBO traer el id del user ME falta
-        products : JSON.parse(localStorage.getItem('cartProducts')) || [],
-    })
-
-    let subTotal    = 0;
-    
-    const shopping = async () => {
-        console.log(paymentData);
-        try {
-            const { data } = await axios.post(`${VITE_SERVER_URL}/orders`, paymentData);
-            window.location.href = data;
-            console.log();
-        } catch (error) {
-            alert("error: " + error)
-        }
+  const authData = useSelector((state) => state.authData);
+  const [paymentData, setPaymentData] = useState({
+    userId: authData?.id || null, //!DEBO traer el id del user ME falta
+    products: JSON.parse(localStorage.getItem("cartProducts")) || [],
+  });
+  const shopping = async () => {
+    try {
+      const { data } = await axios.post(
+        `${VITE_SERVER_URL}/orders`,
+        paymentData
+      );
+      window.location.href = data;
+    } catch (error) {
+      alert("error: " + error);
     }
+  };
 
-    const handleQuantityChange = (productId, action) => {
-        const updatedProducts = paymentData.products.map(product => {
-            if (product.id === productId) {
-                const newQuantity = action === 'add' ? product.quantity + 1 : Math.max(product.quantity - 1, 1);
-                return { ...product, quantity: newQuantity };
-            }
-            return product;
-        });
+  const handleQuantityChange = (productId, action) => {
+    const updatedProducts = paymentData.products.map((product) => {
+      if (product.id === productId) {
+        const newQuantity =
+          action === "add"
+            ? product.quantity + 1
+            : Math.max(product.quantity - 1, 1);
+        return { ...product, quantity: newQuantity };
+      }
+      return product;
+    });
 
-        setPaymentData(prevData => ({
-            ...prevData,
-            products: updatedProducts,
-        }));
-        localStorage.setItem('cartProducts', JSON.stringify(updatedProducts));
-    };
+    setPaymentData((prevData) => ({
+      ...prevData,
+      products: updatedProducts,
+    }));
+    localStorage.setItem("cartProducts", JSON.stringify(updatedProducts));
+  };
 
-    const handleRemoveItem = (productId) => {
-        const updatedProducts = paymentData.products.filter(product => product.id !== productId);
-        setPaymentData(prevData => ({
-            ...prevData,
-            products: updatedProducts,
-        }));
-        localStorage.setItem('cartProducts', JSON.stringify(updatedProducts));
-    };
+  const handleRemoveItem = (productId) => {
+    const updatedProducts = paymentData.products.filter(
+      (product) => product.id !== productId
+    );
+    setPaymentData((prevData) => ({
+      ...prevData,
+      products: updatedProducts,
+    }));
+    localStorage.setItem("cartProducts", JSON.stringify(updatedProducts));
+  };
 
-    const handleRemoveAll = () => {
-        setPaymentData(prevData => ({
-            ...prevData,
-            products: [],
-        }));
-        localStorage.removeItem('cartProducts');
-    };
+  const handleRemoveAll = () => {
+    setPaymentData((prevData) => ({
+      ...prevData,
+      products: [],
+    }));
+    localStorage.removeItem("cartProducts");
+  };
 
-    return (
-        <div className={style.cart}>
-            <div className={style.cartContainer}>
-                <div className={style.header}>
-                    <h1>Shopping Cart</h1>
-                    <h2 onClick={handleRemoveAll}>Remove All</h2>
-                </div>
-                
-
-                {paymentData.products?.map(product => 
-                    <div key = { product.id } className={style.cartLine}>
-                        <img src={product.picture_url} alt=""/>
-                        <div  className={style.cartTitCon}> { product.title } </div>
-                        <div>
-                            <button onClick={() => handleQuantityChange(product.id, 'subtract')}>-</button>
-                            {product.quantity}
-                            <button onClick={() => handleQuantityChange(product.id, 'add')}>+</button>
-                        </div>
-                        <div> 
-                            { product.unit_price.toFixed(2) }
-                            <span className={style.removeItem}  onClick={() => handleRemoveItem(product.id)}>
-                                <BsFillTrash3Fill/>
-                            </span>
-                        </div>
-                    </div>
-                )}
-
-                {!paymentData.products.length && <div> You don't have any products in your shopping cart</div>}
-
-                <div className={style.totalPrice}>
-                    Sub Total : $
-                    {subTotal = paymentData.products.reduce((total, product) => total + (product.unit_price * product.quantity), 0).toFixed(2)}
-                    <button 
-                    disabled={!authData?.id}
-                    onClick={() => shopping()}>CheckOut</button>
-                </div>
-
-            </div>
+  return (
+    <div className={style.cart}>
+      <div className={style.cartContainer}>
+        <div className={style.header}>
+          <h1>Shopping Cart</h1>
+          <h2 onClick={handleRemoveAll}>Remove All</h2>
         </div>
-    )
-}
+
+        {paymentData.products?.map((product) => (
+          <div key={product.id} className={style.cartLine}>
+            <img src={product.picture_url} alt="" />
+            <div className={style.cartTitCon}> {product.title} </div>
+            <div>
+              <button
+                onClick={() => handleQuantityChange(product.id, "subtract")}
+              >
+                -
+              </button>
+              {product.quantity}
+              <button onClick={() => handleQuantityChange(product.id, "add")}>
+                +
+              </button>
+            </div>
+            <div>
+              {product.unit_price.toFixed(2)}
+              <span
+                className={style.removeItem}
+                onClick={() => handleRemoveItem(product.id)}
+              >
+                <BsFillTrash3Fill />
+              </span>
+            </div>
+          </div>
+        ))}
+
+        {!paymentData.products.length && (
+          <div> You don&apos;t have any products in your shopping cart</div>
+        )}
+
+        <div className={style.totalPrice}>
+          Sub Total : $
+          {
+            (paymentData.products
+              .reduce(
+                (total, product) =>
+                  total + product.unit_price * product.quantity,
+                0
+              )
+              .toFixed(2))
+          }
+          <button disabled={!authData?.id} onClick={() => shopping()}>
+            CheckOut
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};

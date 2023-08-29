01bb356cbb5bda0ea96bd3b21855b0719a89aaf4
Wilmer Andres Soto A <113364120+WilSotoA@users.noreply.github.com>
2023-08-29 10:43:55 -0500

    Revert "Chatbot"

--- client/src/components/Chatbot/BotAvatar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles/BotAvatar.css";
-
-// overwrite the existing avatar
-
-export const BotAvatar = () => {
-  return <div className="chatbot-avatar"> GL </div>;
-};

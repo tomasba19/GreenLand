7820f6e804d62aad486db44aa8f02db2008f9529
programador5781 <programador5781@gmail.com>
2023-08-28 22:31:15 -0500

    Create Chatbot
    creando el componente Chatbot, usando la libreria react-chatbot-kit

---
+++ client/src/components/Chatbot/Chatbot.jsx
@@ -0,0 +1,34 @@
+import ChatBot from 'react-chatbot-kit'
+import style from './styles/Chatbot.css';
+import 'react-chatbot-kit/build/main.css'
+
+
+import { config } from './config';
+import { MessageParser } from './MessageParser';
+import { ActionProvider } from './ActionProvider';
+
+export const Chatbot = () => {
+  const validator = (input) => {
+    // deshabilita el botón si quieren enviar un mensaje vacío
+    if (input.length >= 3) return true;
+    return false
+  }
+  
+  return (
+    <div className='chatContainer'>
+      <div className='chatHeader'>
+        <ChatBot
+          config={config}
+          messageParser={MessageParser}
+          // actionProvider={ActionProvider}
+          actionProvider={ActionProvider}
+          validator={validator}
+          style={style}
+        />
+      </div>
+
+    </div>
+  );
+};
+
+

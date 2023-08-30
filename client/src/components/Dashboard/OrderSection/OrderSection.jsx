86c291cdb1ca5e77a39e812291e63efe5cb3f923
Tomas Barolo <tomasba19@gmail.com>
2023-08-30 16:36:15 -0300

    DashboardOrders in progress

--- client/src/components/Dashboard/OrderSection/OrderSection.jsx
+++ client/src/components/Dashboard/OrderSection/OrderSection.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import style from './OrderSection.module.css'
+import { OrderTable } from './OrderTable'
 
 
 
 
 export const OrderSection = () => {
+  const ordersData = [
+    { id: 1, date: 19/3/99, totalPrice: 222.6, status: 'approved', userId: 3 },
+  
+  ]
   return (
     <div className={style.OrderSection}>
-        <h3>Aca va la tabla de orders</h3>
+        <h2>Orders</h2>
+        <OrderTable ordersData={ordersData} />
     </div>
   )
 }

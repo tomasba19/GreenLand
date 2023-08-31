290cdf1a083c9915f2c8e1eed70b2437df5ea41d
Wilmer Andres Soto A <113364120+WilSotoA@users.noreply.github.com>
2023-08-30 19:25:34 -0500

    Include patch method

--- api/src/middlewares/cors.js
+++ api/src/middlewares/cors.js
@@ -10,7 +10,7 @@ router.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin)
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   }
 
   next()

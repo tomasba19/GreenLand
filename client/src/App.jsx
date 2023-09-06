26b97d62b01bfafa1583eb791acb63841a835465
Tomas Barolo <tomasba19@gmail.com>
2023-09-06 20:01:57 -0300

    Admin Restriction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,46 @@
-import { useEffect } from "react"
-import { Route, Routes, useLocation } from "react-router-dom"
-import { Navigate } from "react-router-dom"
-import { useSelector } from "react-redux/es/hooks/useSelector"
-import { NavBar } from "./components/NavBar/NavBar"
-import { Home } from "./components/Home/Home"
-import { Footer } from "./components/footer/Footer"
-import { Products } from "./components/Products/Products"
-import { Login } from "./components/Login/Login"
-import { SignUp } from "./components/SignUp/SignUp"
-import { About } from "./components/About/About"
-import { Form } from "./components/Form/Form"
-import { Detail } from "./components/Detail/Detail"
-import { ContactUs } from "./components/ContactUs/ContactUs"
-import { Landing } from "./components/Landing/Landing"
-import { ShoppingCart } from "./components/ShoppingCart/ShoppingCart"
-import { WhisList } from "./components/Whislist/Whislist"
-import { useDispatch } from "react-redux"
-import { authData, logout } from "./redux/action"
-import decode from "jwt-decode"
-import { UserProfile } from "./components/UserProfile/UserProfile"
-import { OrderSection } from "./components/Dashboard/OrderSection/OrderSection"
-import { DashboardLayout } from "./components/Dashboard/DashboardLayout"
-import { MainDash } from "./components/Dashboard/MainDash/MainDash"
-import { CustomerSection } from "./components/Dashboard/CustomerSection/CustomerSection"
-import { ProductSection } from "./components/Dashboard/ProductSection/ProductSection"
-import AnalyticSection from "./components/Dashboard/AnalyticSection/AnalyticSection"
-import { UndoPurchaseForm } from "./components/UndoPurchaseForm/UndoPurchaseForm"
-import { NotFound404 } from "./components/NotFound404/NotFound404"
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Home } from "./components/Home/Home";
+import { Footer } from "./components/footer/Footer";
+import { Products } from "./components/Products/Products";
+import { Login } from "./components/Login/Login";
+import { SignUp } from "./components/SignUp/SignUp";
+import { About } from "./components/About/About";
+import { Form } from "./components/Form/Form";
+import { Detail } from "./components/Detail/Detail";
+import { ContactUs } from "./components/ContactUs/ContactUs";
+import { Landing } from "./components/Landing/Landing";
+import { ShoppingCart } from "./components/ShoppingCart/ShoppingCart";
+import { WhisList } from "./components/Whislist/Whislist";
+import { useDispatch } from "react-redux";
+import { authData, logout } from "./redux/action";
+import decode from "jwt-decode";
+import { UserProfile } from "./components/UserProfile/UserProfile";
+import { OrderSection } from "./components/Dashboard/OrderSection/OrderSection";
+import { DashboardLayout } from "./components/Dashboard/DashboardLayout";
+import { MainDash } from "./components/Dashboard/MainDash/MainDash";
+import { CustomerSection } from "./components/Dashboard/CustomerSection/CustomerSection";
+import { ProductSection } from "./components/Dashboard/ProductSection/ProductSection";
+import AnalyticSection from "./components/Dashboard/AnalyticSection/AnalyticSection";
+import { UndoPurchaseForm } from "./components/UndoPurchaseForm/UndoPurchaseForm";
+import { NotFound404 } from "./components/NotFound404/NotFound404";
 
 function App() {
-  const location = useLocation()
-  const dispatch = useDispatch()
-  const userLogged = JSON.parse(localStorage.getItem("profile"))
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const userLogged = JSON.parse(localStorage.getItem("profile"));
 
   useEffect(() => {
-    const storedProfile = JSON.parse(localStorage.getItem("profile"))
+    const storedProfile = JSON.parse(localStorage.getItem("profile"));
     if (storedProfile) {
-      const decodedToken = decode(storedProfile?.token)
-      if (decodedToken.exp * 1000 < new Date().getTime()) dispatch(logout)
-      else dispatch(authData(storedProfile))
+      const decodedToken = decode(storedProfile?.token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) dispatch(logout);
+      else dispatch(authData(storedProfile));
     }
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
     /*Estilos para el scroll-bar, pendiente de modularizar*/
@@ -59,49 +59,74 @@ function App() {
         <Route path="/login/:verificado?" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/form" element={<Form />} />
+        <Route
+          path="/form"
+          element={
+            userLogged && userLogged.user?.id === 1 ? <Form /> : <NotFound404 />
+          }
+        />
         <Route path="/detail/:id" element={<Detail />} />
-        
+
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/wishlist" element={<WhisList />} />
         <Route
           path="/dashboard"
           element={
-            <DashboardLayout>
-              <MainDash />
-            </DashboardLayout>
+            userLogged && userLogged.user?.id === 1 ? (
+              <DashboardLayout>
+                <MainDash />
+              </DashboardLayout>
+            ) : (
+              <NotFound404 />
+            )
           }
         />
         <Route
           path="/dashboard/orders"
           element={
-            <DashboardLayout>
-              <OrderSection />
-            </DashboardLayout>
+            userLogged && userLogged.user?.id === 1 ? (
+              <DashboardLayout>
+                <OrderSection />
+              </DashboardLayout>
+            ) : (
+              <NotFound404 />
+            )
           }
         />
         <Route
           path="/dashboard/customers"
           element={
-            <DashboardLayout>
-              <CustomerSection />
-            </DashboardLayout>
+            userLogged && userLogged.user?.id === 1 ? (
+              <DashboardLayout>
+                <CustomerSection />
+              </DashboardLayout>
+            ) : (
+              <NotFound404 />
+            )
           }
         />
         <Route
           path="/dashboard/products"
           element={
-            <DashboardLayout>
-              <ProductSection />
-            </DashboardLayout>
+            userLogged && userLogged.user?.id === 1 ? (
+              <DashboardLayout>
+                <ProductSection />
+              </DashboardLayout>
+            ) : (
+                <NotFound404 />
+            )
           }
         />
         <Route
           path="/dashboard/analytics"
           element={
-            <DashboardLayout>
-              <AnalyticSection />
-            </DashboardLayout>
+            userLogged && userLogged.user?.id === 1 ? (
+              <DashboardLayout>
+                <AnalyticSection />
+              </DashboardLayout>
+            ) : (
+              <NotFound404 />
+            )
           }
         />
         <Route path="/undo-purchase" element={<UndoPurchaseForm />} />
@@ -117,7 +142,7 @@ function App() {
         location.pathname !== "/dashboard/products" &&
         location.pathname !== "/dashboard/analytics" && <Footer />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
